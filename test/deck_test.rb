7a29c916d83c4d232c13a63b9902bebc2124751a
Aidan McGuire <>
2021-02-01 19:30:08 -0700

    Refactor test_high_ranking-cards to test edge cases

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -47,9 +47,11 @@ class DeckTest < Minitest::Test
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
     card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
+    card4 = Card.new(:spade, 'Jack', 11)
+    card5 = Card.new(:clubs, '10', 10)
+    cards = [card1, card2, card3, card4, card5]
     deck = Deck.new(cards)
-    assert_equal [card1, card3], deck.high_ranking_cards
+    assert_equal [card1, card3, card4], deck.high_ranking_cards
   end
 
   def test_percent_high_ranking
