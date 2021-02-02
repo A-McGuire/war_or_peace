10bcb5ee044ee0b9f4f2817915abcf4e9c24c113
Aidan McGuire <>
2021-02-01 19:03:15 -0700

    Add percent_high_ranking method w/ test

--- lib/deck.rb
+++ lib/deck.rb
@@ -2,20 +2,20 @@ class Deck
   attr_reader :cards
   def initialize(cards = [])
     @cards = cards
-    # require "pry"; binding.pry
+    @high_cards = 0
   end
 
   def rank_of_card_at(index)
-    # require "pry"; binding.pry
     @cards[index].rank
   end
 
   def high_ranking_cards
-
+    @high_cards = @cards.select {|card| card.rank > 10}
   end
 
   def percent_high_ranking
-
+    percentage = @high_cards.length.to_f / cards.length * 100
+    percentage.round(2)
   end
 
   def remove_card
