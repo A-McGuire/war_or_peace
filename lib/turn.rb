81bbf2b6b9b39b519415135ee897e59f5baa336c
Aidan McGuire <>
2021-02-05 19:23:55 -0700

    Replace instance variable calls with methods

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,23 +7,23 @@ class Turn
   end
 
   def type #determines type of battle and removes card. see deck>remove_card method
-    if @player1.deck.rank_of_card_at(0) != @player2.deck.rank_of_card_at(0)
+    if player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)
       :basic
-    elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0)
+    elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
       :war
     end
   end
 
   def winner #determines winner of turn
-    if @player1.deck.rank_of_card_at(0) > @player2.deck.rank_of_card_at(0)
-      @player1
+    if player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
+      player1
     else
-      @player2
+      player2
     end
   end
 
   def pile_cards
-    @spoils_of_war.push(@player1.deck.remove_card, @player2.deck.remove_card)
+    spoils_of_war.push(player1.deck.remove_card, player2.deck.remove_card)
   end
 
   def award_spoils
